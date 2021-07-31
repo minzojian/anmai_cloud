180fe90010c390d683f0a702462e392bf277f0c5
KentWood <minzojian@hotmail.com>
2021-07-31 18:22:29 +0800

    完善了一些细节

--- domain.js
+++
@@ -1,4 +0,0 @@
-var baseNode = document.createElement("base")
-baseNode.href =
-  "http://static-1251577693.cos.ap-chengdu.myqcloud.com/projects/h5/anmai_cloud_mobile/"
-document.head.append(baseNode)
